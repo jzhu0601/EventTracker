004e1f6692f2a3d927d083172cedc991f078e945
jzhu0601 <jzhu@comresource.com>
2015-07-14 10:20:54 -0400

    first commit

---
+++ src/main/java/com/comresource/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.comresource.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping(value="/greeting")
+	public String sayHello(Model model){
+		model.addAttribute("greeting", "hello controller");
+		
+		return "hello";
+	}
+	
+	@RequestMapping(value="/index")
+	public String index(Model model){
+		return "forward:index.jsp";
+	}
+}
